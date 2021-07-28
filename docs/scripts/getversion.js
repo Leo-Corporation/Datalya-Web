d62c2b692fbbfb9bf5adf32449f5b66523795a1b
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-07-28 14:38:02 +0200

    Added download page

---
+++ docs/scripts/getversion.js
@@ -0,0 +1,12 @@
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false);
+    xmlHttp.send(null);
+    console.log("HTTP Result:\n\n" + xmlHttp.responseText);
+    return xmlHttp.responseText;
+}
+
+const vUrl = "https://raw.githubusercontent.com/Leo-Corporation/LeoCorp-Docs/master/Liens/Update%20System/Datalya/Version.txt";
+
+var response = httpGet(vUrl);
+document.getElementById("downloadTxt").innerHTML = "Version " + response;
