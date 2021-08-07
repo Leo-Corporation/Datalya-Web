2fa2bba6f3ad9976838a45bb5ead3c69c347d153
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-08-07 18:28:07 +0200

    Replaced XMLHttpRequest with fetch()

--- docs/scripts/getversion.js
+++ docs/scripts/getversion.js
@@ -1,12 +1,8 @@
-function httpGet(theUrl) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", theUrl, false);
-    xmlHttp.send();
-    console.log("HTTP Result:\n\n" + xmlHttp.responseText);
-    return xmlHttp.responseText;
+async function getVersion(url) {
+    let response = await fetch(vUrl);
+    let data = await response.text();
+    document.getElementById("downloadTxt").innerHTML = "Version " + data;
 }
 
 const vUrl = "https://raw.githubusercontent.com/Leo-Corporation/LeoCorp-Docs/master/Liens/Update%20System/Datalya/Version.txt";
-
-var response = httpGet(vUrl);
-document.getElementById("downloadTxt").innerHTML = "Version " + response;
+getVersion(vUrl);
