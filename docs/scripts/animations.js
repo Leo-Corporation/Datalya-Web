da1be2438b25ecfac993720b06285eaad5eeaf5c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-08-12 12:03:49 +0200

    Added animations

---
+++ docs/scripts/animations.js
@@ -0,0 +1,16 @@
+function animateOnView() {
+    const observer = new IntersectionObserver(entries => {
+        // Loop over the entries
+        entries.forEach(entry => {
+            // If the element is visible
+            if (entry.isIntersecting) {
+                // Add the animation class
+                entry.target.classList.add('fadeIn');
+                entry.target.classList.remove('opacity-0');
+            }
+        });
+    });
+    observer.observe(document.getElementsByClassName("onViewedAnimation")[0]);
+}
+
+animateOnView();
